30f484b88cddbd1f2121bae92dd46b384e74cc90
iigunchev <iigunchev@gmail.com>
2022-03-06 16:01:36 +0100

    fetch and display data from api

--- advice-generator-app-main/advice-generator/src/App.js
+++ advice-generator-app-main/advice-generator/src/App.js
@@ -1,15 +1,28 @@
+import { useState, useEffect } from "react";
 import './App.css';
 import iconDice from "./images/icon-dice.svg";
 import desktopDivider from "./images/pattern-divider-desktop.svg";
 
 function App() {
+
+  const [advice, setAdvice] = useState({})
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    const {id, advice} = data.slip;
+    setAdvice({advice: advice, id: id})
+  }
+  useEffect(() => {
+    getAdvice();
+  }, [])
+  
   return (
     <div className="container">
       <article>
-        <h3>ADVICE  # 117</h3>
-        <h1>"It's easy to sit up and take notice, what's difficult is getting up and taking action."</h1>
+        <h3>ADVICE  # {advice.id}</h3>
+        <h1>{advice.advice}</h1>
         <img src={desktopDivider} alt="divider" className="divider"/>
-        <button><img src={iconDice} alt="dice icon"/></button>
+        <button onClick={() => getAdvice()}><img src={iconDice} alt="dice icon"/></button>
       </article>
     </div>
   );
