1e26f66393c7a59d4c47048e266cb33ecdbd1a41
iigunchev <iigunchev@gmail.com>
2022-03-06 21:16:46 +0100

    mobile version

--- advice-generator-app-main/advice-generator/src/App.js
+++ advice-generator-app-main/advice-generator/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import './App.css';
 import iconDice from "./images/icon-dice.svg";
 import desktopDivider from "./images/pattern-divider-desktop.svg";
+import mobileDivider from "./images/pattern-divider-mobile.svg";
 
 function App() {
 
@@ -19,9 +20,9 @@ function App() {
   return (
     <div className="container">
       <article>
-        <h3>ADVICE  # {advice.id}</h3>
-        <h1>{advice.advice}</h1>
-        <img src={desktopDivider} alt="divider" className="divider"/>
+        <h3>ADVICE  #{advice.id}</h3>
+        <h1>"{advice.advice}"</h1>
+        <div className="divider" />
         <button onClick={() => getAdvice()}><img src={iconDice} alt="dice icon"/></button>
       </article>
     </div>
